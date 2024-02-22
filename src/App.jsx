81efb8c3f771cc3b769ae866c319a8cb77f8d4fd
sakiynam <mamatisayevasarvinoz@gmail.com>
2024-02-23 00:00:05 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import logo from './assets/Logo.svg'
 import Fox from './assets/fox.svg'
-import {Header, Container, Navbar} from '../style'
+import {Header, Container, Navbar, Contact, Banner, Info, Join} from '../style'
 
 
 function App() {
 
   return (
     <>
+     <Container>
      <Header>
-      <Container>
         <div>
           <img src={logo} alt="" />
         </div>
@@ -18,20 +18,18 @@ function App() {
           <a href="#">About us</a>
           <a href="#">Locations</a>
           <a href="#">FAQ's</a>
-          <a href="#">Contact us</a>
+          <Contact> Contact Us</Contact>
         </Navbar>
-      </Container>
      </Header>
-     {/* <Banner>
+     <Banner>
       <Info>
         <h1>Love and kindness are never wasted.</h1>
         <p>We help around 1,00,000+ cats and kittens every year thanks in no small part to our network of over 200+ volunteer-run branches.</p>
-        <button>Join Us</button>
+        <Join>Join Us</Join>
       </Info>
-      <Image>
-        <img src={Fox} alt="" />
-      </Image>
-     </Banner> */}
+        <img src={Fox} alt="" className='fox' />
+     </Banner>
+     </Container>
     </>
   )
 }
--- style.js
+++ style.js
@@ -1,19 +1,53 @@
 import styled from "styled-components";
 
 const Header = styled.header`
-   width: 100%;
-`
+    width: 100%;
+    padding: 30px 0 0 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+   `
 
 const Container = styled.div`
-    max-width: 1110px;
-    display: flex;
-    justify-content: space-around;
+    max-width: 1120px;
+    margin: 0 auto;
+    
+
 `
 
 const Navbar = styled.div`
     display: flex;
-
+    gap: 30px;
+    align-items: center;
 `
 
+const Contact = styled.button`
+    background-color: #6DFC56;
+    width: 130px;
+    height: 42px;
+    border: 1px solid rgb(19, 57, 13);
+    border-radius: 4px;
+`
 
-export {Header, Container, Navbar};
+const Banner = styled.div`
+    display: flex;
+`
+
+const Info = styled.div`
+    width: 650px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 108px 0;
+`
+
+const Join = styled.button`
+    width: 200px;
+    height: 60px;
+    border-radius: 4px;  
+    background: rgb(19, 57, 13);
+    color: rgb(255, 255, 255);
+    `
+
+
+export {Header, Container, Navbar, Contact, Banner, Info, Join};
